f23ea9fc52c4dfc1872e5dfbf30db4b499a3c028
codecraftman77654 <kk1551737@gmail.com>
2026-02-13 17:00:13 +0530

    types of datatypes

---
+++ Python/var.py
@@ -0,0 +1,6 @@
+a=1 # it ia an integer datatype
+b=8.5575 # it is a floating type datatype
+c="kuldeep kashyap" # it is a string type datatype
+d=False # it is a boolean type datatype
+e=None # it is a none type datatype
+print(a,b,c,d,e)
