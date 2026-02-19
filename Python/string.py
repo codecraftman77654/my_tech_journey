72a898e0c21e8562ada0ab29dc64f48c166f9369
codecraftman77654 <kk1551737@gmail.com>
2026-02-19 17:55:14 +0530

    some frequently used strings in python

---
+++ Python/string.py
@@ -0,0 +1,18 @@
+name = "kuldeep"
+print(len(name)) # it tells the length of the string
+
+string ="abrakadabra"
+count=string.count("k")
+print(count)  # it counts the repetation of any alphabet in a word or string
+
+s="kuldeep"
+capitalized_string=s.capitalize()
+print(capitalized_string) # it makes first alphabet capital
+
+a="hello world"
+index=a.find("world")
+print(index) #it gives the output at which the alphabet starts.... as here world starte on 6th position
+
+b="kuldeep is a bad boy"
+replaced_string=b.replace("bad","good")
+print(replaced_string) # it changes the word which is selected
