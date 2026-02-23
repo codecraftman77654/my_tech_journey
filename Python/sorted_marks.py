3e5027be36a1bf21216b16ae0868298f42bd0150
codecraftman77654 <kk1551737@gmail.com>
2026-02-23 16:15:51 +0530

    tuples and lists

---
+++ Python/sorted_marks.py
@@ -0,0 +1,19 @@
+marks = []
+
+s1 = int(input("Enter marks of first student : "))
+marks.append(s1)
+
+s2 = int(input("Enter marks of second student : "))
+marks.append(s2)
+
+s3 = int(input("Enter marks of third student : "))
+marks.append(s3)
+
+s4 = int(input("Enter marks of fourth student : "))
+marks.append(s4)
+
+s5 = int(input("Enter marks of fifth student : "))
+marks.append(s5)
+
+marks.sort()
+print(marks)
---
+++ Python/youple_type.py
@@ -0,0 +1,5 @@
+a = (34, 234, "harry")
+
+a[2] = "carry"
+
+# tuple does not support the item assignment. 
