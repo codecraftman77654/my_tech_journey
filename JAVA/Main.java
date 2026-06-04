95637a228e1101e873edd6f917e0e1a0b1394b65
codecraftman77654 <kk1551737@gmail.com>
2026-06-04 21:05:09 +0530

    initial commit

---
+++ JAVA/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean passed = true;
+        String name = " Lucifer";
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(passed);
+        System.out.println(name);
+
+    }
+}
