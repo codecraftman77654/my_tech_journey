da84623c2a8352e9953835e2f5f6af390fae0589
codecraftman77654 <kk1551737@gmail.com>
2026-06-04 21:16:48 +0530

    initial commit

---
+++ JAVA/sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        int result = a + b;
+        System.out.println("result=" + result);
+    }
+}
