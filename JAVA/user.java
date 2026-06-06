c68eba7c05425f71aa2b5e158cb2810d0f5c9850
codecraftman77654 <kk1551737@gmail.com>
2026-06-06 19:32:02 +0530

    initial commit

---
+++ JAVA/user.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class user {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
