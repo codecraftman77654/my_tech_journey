0260cdb6dccf0ac62bb2732082e39e878842da57
codecraftman77654 <kk1551737@gmail.com>
2026-06-10 13:44:45 +0530

    initial commit

---
+++ JAVA/student_report.java
@@ -0,0 +1,16 @@
+class student_report {
+    public static void main(String[] args) {
+        String name = "kuldeep";
+        int roll = 12370;
+        double marks = 88.5;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("---------- Student Report----------");
+        System.out.println("Name : " + name);
+        System.out.println("Roll No : " + roll);
+        System.out.println("MARKS : " + marks);
+        System.out.println("Grade : " + grade);
+        System.out.println("Passed : " + passed);
+    }
+}
