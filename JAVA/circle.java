d3aa4ec3ec45375c0756c673191934231c863cd8
codecraftman77654 <kk1551737@gmail.com>
2026-06-10 09:32:55 +0530

    initial commit

---
+++ JAVA/circle.java
@@ -0,0 +1,11 @@
+class circle {
+    public static void main(String[] args) {
+        int radius = 5;
+        double pi = 3.147;
+
+        double area = pi * radius * radius;
+
+        System.out.print(area);
+
+    }
+}
