8b083a2776a8b5d82c9ec85607aa910e83a375fa
codecraftman77654 <kk1551737@gmail.com>
2026-06-10 09:21:49 +0530

    initial commit

---
+++ JAVA/rectangle.java
@@ -0,0 +1,10 @@
+class rectangle {
+    public static void main(String[] args) {
+        double length = 7.25;
+        double breadth = 5.25;
+        double area = length * breadth;
+
+        System.out.print(area);
+
+    }
+}
