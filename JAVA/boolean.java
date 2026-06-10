6d22c8feb8a16725fe56c71ea1fd8b8fc6e87778
codecraftman77654 <kk1551737@gmail.com>
2026-06-10 12:09:38 +0530

    initial commit

---
+++ JAVA/boolean.java
@@ -0,0 +1,6 @@
+class bool {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        System.out.println("is Java Fun?" + isJavaFun);
+    }
+}
---
+++ JAVA/character.java
@@ -0,0 +1,7 @@
+class character {
+    public static void main(String[] args) {
+        char grade = 'A';
+
+        System.out.print("MY GRADE IS " + grade);
+    }
+}
