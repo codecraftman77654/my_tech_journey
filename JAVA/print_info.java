028f675bbf77873a8a1bffa07053722b0db8085c
codecraftman77654 <kk1551737@gmail.com>
2026-06-10 09:11:24 +0530

    initial commit

---
+++ JAVA/print_info.java
@@ -0,0 +1,11 @@
+class print_info {
+    public static void main(String[] args) {
+        String name = "kuldeep";
+        int age = 22;
+        String city = "katihar";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
