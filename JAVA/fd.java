1b36739ba10b6f4f51428c1a490fffcc84f1dc66
codecraftman77654 <kk1551737@gmail.com>
2026-06-10 12:24:49 +0530

    initial commit

---
+++ JAVA/fd.java
@@ -0,0 +1,9 @@
+class fd {
+    public static void main(String[] args) {
+        float num1 = 3.14f;
+        double num2 = 3.14159265359;
+
+        System.out.println(num1);
+        System.out.print(num2);
+    }
+}
