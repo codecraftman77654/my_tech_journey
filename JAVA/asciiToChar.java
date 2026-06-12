086c71e526ba8b295b70c571e44633f41555696c
codecraftman77654 <kk1551737@gmail.com>
2026-06-12 20:08:37 +0530

    initial commit

---
+++ JAVA/asciiToChar.java
@@ -0,0 +1,10 @@
+class asciiToChar {
+    public static void main(String[] args) {
+        int ascii = 70;
+        char ch = (char) ascii;
+
+        System.out.println(ch);
+
+    }
+
+}
---
+++ JAVA/charToAscii.java
@@ -0,0 +1,8 @@
+public class charToAscii {
+    public static void main(String[] args) {
+        char c = 'K';
+        int ascii = c;
+
+        System.out.println(ascii);
+    }
+}
