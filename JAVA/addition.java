62244a1ae594d83eb40290f4a76deb08752e646b
codecraftman77654 <kk1551737@gmail.com>
2026-06-14 13:09:02 +0530

    initial commit

---
+++ JAVA/addition.java
@@ -0,0 +1,8 @@
+class addition {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 35;
+        int sum = a + b;
+        System.out.println("The sum is : " + sum);
+    }
+}
