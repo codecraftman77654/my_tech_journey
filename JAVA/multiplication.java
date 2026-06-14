48f5c7301eabd71a4135ef4c751106036ac0a420
codecraftman77654 <kk1551737@gmail.com>
2026-06-14 13:21:07 +0530

    initial commit

---
+++ JAVA/multiplication.java
@@ -0,0 +1,8 @@
+class multiplication {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 17;
+        int product = a * b;
+        System.out.println(product);
+    }
+}
