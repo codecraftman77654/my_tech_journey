ed90d89fcf425947a114aecf15feb780d7f41577
codecraftman77654 <kk1551737@gmail.com>
2026-06-14 13:53:01 +0530

    initial commit

---
+++ JAVA/average.java
@@ -0,0 +1,12 @@
+class average {
+
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 17;
+        int num = 29;
+
+        double average = (num + num1 + num2) / 3.0;
+
+        System.out.println(average);
+    }
+}
