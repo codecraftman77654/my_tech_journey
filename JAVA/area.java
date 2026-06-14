d3ec9105fbceac0fae1d090acfeedd2a4d3083b6
codecraftman77654 <kk1551737@gmail.com>
2026-06-14 13:41:24 +0530

    initial commit

---
+++ JAVA/area.java
@@ -0,0 +1,9 @@
+class area {
+    public static void main(String[] args) {
+        int length = 7;
+        int bridth = 3;
+        int area = length * bridth;
+        System.out.println("the area of rectangle is " + area + " cm square");
+
+    }
+}
