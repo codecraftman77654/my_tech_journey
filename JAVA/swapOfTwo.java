2cb13071106ed8266f28cd1596e6e754f31bdb40
codecraftman77654 <kk1551737@gmail.com>
2026-06-16 21:49:08 +0530

    initial commit

---
+++ JAVA/swapOfTwo.java
@@ -0,0 +1,13 @@
+class swapOfTwo {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 35;
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+    }
+}
