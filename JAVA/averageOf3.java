456806ed2e37467bbeeaf4344fc7d3ef9c0b1ab7
codecraftman77654 <kk1551737@gmail.com>
2026-06-16 21:34:56 +0530

    initial commit

---
+++ JAVA/averageOf3.java
@@ -0,0 +1,11 @@
+class averageOf3 {
+    public static void main(String[] args) {
+        double num1 = 17;
+        double num2 = 27;
+        double num3 = 147;
+
+        double average = (num1 + num2 + num3) / 3;
+
+        System.out.println(" Average of Three Numbers Are : " + average);
+    }
+}
