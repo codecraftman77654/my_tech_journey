b405b3ba6e2f0399c5bacf6463c59cf31701b363
codecraftman77654 <kk1551737@gmail.com>
2026-06-16 21:29:26 +0530

    initial commit

---
+++ JAVA/area2.java
@@ -0,0 +1,9 @@
+class area2 {
+    public static void main(String[] args) {
+        double radius = 7;
+        double pi = 3.147;
+        double area = pi * radius * radius;
+
+        System.out.println(area);
+    }
+}
