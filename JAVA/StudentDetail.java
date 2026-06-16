46802cde19d7d5b50094efb9b0ee044b9cd02e77
codecraftman77654 <kk1551737@gmail.com>
2026-06-16 22:04:47 +0530

    initial commit

---
+++ JAVA/StudentDetail.java
@@ -0,0 +1,15 @@
+class StudentDetail {
+    public static void main(String[] args) {
+
+        String name = "Kuldeep";
+        int age = 22;
+        double percentage = 78.50;
+        char grade = 'B';
+
+        System.out.println("NAME : " + name);
+        System.out.println("AGE : " + age);
+        System.out.println("PERCENTAGE : " + percentage);
+        System.out.println("GRADE : " + grade);
+
+    }
+}
---
+++ JAVA/compare.java
@@ -0,0 +1,7 @@
+class compare {
+    public static void main(String[] args) {
+        int result = (35 > 51) ? 35 : 51;
+
+        System.out.println(" The  larger number is : " + result);
+    }
+}
