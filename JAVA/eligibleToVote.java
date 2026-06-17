5e3052752f6129b25335fad434d1b305749a1cb5
codecraftman77654 <kk1551737@gmail.com>
2026-06-17 10:38:33 +0530

    initial commmit

---
+++ JAVA/eligibleToVote.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class eligibleToVote {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age : ");
+        int age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Eligible To Vote");
+
+        }
+    }
+}
