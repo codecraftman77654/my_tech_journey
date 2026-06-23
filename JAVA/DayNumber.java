d2947f349508ae90da4c99cc24d696fa88d52725
codecraftman77654 <kk1551737@gmail.com>
2026-06-23 20:15:08 +0530

    compiled day on number programme

---
+++ JAVA/DayNumber.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class DayNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number : ");
+        int num = sc.nextInt();
+
+        if (num == 1) {
+            System.out.println("Monday");
+        }
+
+        else if (num == 2) {
+            System.out.println("Tuesday");
+        }
+
+        else if (num == 3) {
+            System.out.println("Wednesday");
+        }
+
+        else if (num == 4) {
+            System.out.println("Thursday");
+        }
+
+        else if (num == 5) {
+            System.out.println("Friday");
+        }
+
+        else if (num == 6) {
+            System.out.println("Saturday");
+        }
+
+        else if (num == 7) {
+            System.out.println("Sunday");
+        }
+
+        sc.close();
+    }
+}
