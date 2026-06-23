91d9ecea65fac68b06c5fccbe55927b436d84be6
codecraftman77654 <kk1551737@gmail.com>
2026-06-23 19:34:13 +0530

    compiled salary category

--- JAVA/SalaryCategory.java
+++ JAVA/SalaryCategory.java
@@ -1,3 +1,30 @@
 import java.util.Scanner;
 
-class
+class SalaryCategory {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Salary :");
+        int salary = sc.nextInt();
+
+        if (salary == 100000) {
+            System.out.println("Very High");
+        }
+
+        else if (salary == 50000) {
+            System.out.println("High");
+        }
+
+        else if (salary == 30000) {
+            System.out.println("Medium");
+        }
+
+        else if (salary < 30000) {
+            System.out.println("Low");
+        }
+
+        sc.close();
+    }
+}
