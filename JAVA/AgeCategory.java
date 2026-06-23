b09606b3caf727a1fd9774de2a1b38fd969c90cb
codecraftman77654 <kk1551737@gmail.com>
2026-06-23 19:07:43 +0530

    initial commit

---
+++ JAVA/AgeCategory.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter Age : ");
+    int age = sc.nextInt();
+
+    if (age >= 0 & age <= 12) {
+        System.out.println("child");
+    }
+
+    else if (age > 12 & age < 20) {
+        System.out.println("Teenager");
+    }
+
+    else if (age > 19 & age < 60) {
+        System.out.println("Adult");
+    }
+
+    else {
+        System.out.println("Senior Citizen");
+    }
+
+    sc.close();
+
+}
