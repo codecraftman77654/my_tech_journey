cab7096b9a87c2b5d7c886587039b4c6c29a004b
codecraftman77654 <kk1551737@gmail.com>
2026-06-26 18:25:41 +0530

    initial commit

---
+++ JAVA/atmsystem.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class atmsystem {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Card Inserted ?");
+        int card = sc.nextInt();
+
+        System.out.print("Pin correct");
+        int pin = sc.nextInt();
+
+        if (card == yes) {
+            if (pin == 855114) {
+                System.out.println("Proseed")
+            }
+        }
+    }
+}
