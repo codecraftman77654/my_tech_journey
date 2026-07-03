bd39f1db657a4287adf61147909eeb4b1fb82b53
codecraftman77654 <kk1551737@gmail.com>
2026-07-03 12:44:30 +0530

    initial commit

---
+++ JAVA/PrintNumbersDo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class PrintNumbersDo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number : ");
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= n);
+        sc.close();
+    }
+}
