d765c6a2058eeceaa73eaaed226a4f5c568349fa
codecraftman77654 <kk1551737@gmail.com>
2026-07-05 14:02:06 +0530

    initial commit

---
+++ JAVA/EvenFor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class EvenFor {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number :");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++)
+
+        {
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
