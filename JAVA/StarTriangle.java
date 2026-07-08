a82e2939d81d51042fc39384f7629f719b6fc363
codecraftman77654 <kk1551737@gmail.com>
2026-07-08 11:18:33 +0530

    modification made in print function

--- JAVA/StarTriangle.java
+++ JAVA/StarTriangle.java
@@ -1,9 +1,11 @@
 class StarTriangle {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++)
-                System.out.print(i + " ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println();
+
     }
 }
