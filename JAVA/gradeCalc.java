89801fb388f27cee273f87ddf75532ddd1ade54c
codecraftman77654 <kk1551737@gmail.com>
2026-07-09 18:08:37 +0530

    did some improvement with output performance

--- JAVA/gradeCalc.java
+++ JAVA/gradeCalc.java
@@ -5,29 +5,41 @@ class gradeCalc {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter Student Name : ");
+        String name = sc.nextLine();
+
         System.out.print("Enter marks: ");
         int marks = sc.nextInt();
 
-        if (marks > 90) {
-            System.out.println("Grade A ");
-        }
+        if (marks < 0 || marks > 100) {
+            System.out.println("❌ Invalid Marks!");
+            System.out.println("Please enter marks between 0 and 100.");
+        } else {
+            System.out.println("========== RESULT ==========");
+            System.out.println("Name  : " + name);
+            System.out.println("Marks : " + marks);
 
-        else if (marks > 80) {
-            System.out.println("Grade B");
-        }
+            if (marks >= 90) {
+                System.out.println("Grade : A ");
+            }
 
-        else if (marks > 70) {
-            System.out.println("Grade C");
-        }
+            else if (marks >= 80) {
+                System.out.println("Grade : B");
+            }
 
-        else if (marks > 60) {
-            System.out.println("Grade D");
-        }
+            else if (marks >= 70) {
+                System.out.println("Grade : C");
+            }
 
-        else {
-            System.out.println("Fail");
-        }
+            else if (marks >= 60) {
+                System.out.println("Grade : D");
+            }
 
+            else {
+                System.out.println("Grade : F (Fail)");
+            }
+
+        }
         sc.close();
     }
 }
