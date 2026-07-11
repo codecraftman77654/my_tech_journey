770ad813a45509ced29a34c59b418099ea90134f
codecraftman77654 <kk1551737@gmail.com>
2026-07-11 12:54:22 +0530

    initial commit

---
+++ JAVA/CharacterString.java
@@ -0,0 +1,11 @@
+class CharacterString {
+    public static void main(String[] args) {
+
+        char[] alphabet = { 'A', 'B', 'C', 'D' };
+
+        System.out.println(alphabet[0]);
+        System.out.println(alphabet[1]);
+        System.out.println(alphabet[2]);
+        System.out.println(alphabet[3]);
+    }
+}
--- JAVA/compare.java
+++
@@ -1,7 +0,0 @@
-class compare {
-    public static void main(String[] args) {
-        int result = (35 > 51) ? 35 : 51;
-
-        System.out.println(" The  larger number is : " + result);
-    }
-}
