6f23f4c1729795b7794f140f7959111fbaee762f
codecraftman77654 <kk1551737@gmail.com>
2026-07-11 13:10:31 +0530

    initial commit

---
+++ JAVA/StringArray.java
@@ -0,0 +1,12 @@
+class StringArray {
+
+    public static void main(String[] args) {
+        String[] languages = { "java", "python", "C", "javascript" };
+
+        System.out.println(languages[0]);
+        System.out.println(languages[1]);
+        System.out.println(languages[2]);
+        System.out.println(languages[3]);
+
+    }
+}
