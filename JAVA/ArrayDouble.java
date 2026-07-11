537c00e29a4067825bc062e1686015d55e46595b
codecraftman77654 <kk1551737@gmail.com>
2026-07-11 12:41:58 +0530

    initial commit

---
+++ JAVA/ArrayDouble.java
@@ -0,0 +1,14 @@
+class ArrayDouble {
+
+    public static void main(String[] args) {
+
+        double[] num = { 12.5, 25.7, 37.8, 48.2 };
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+        System.out.println(num[4]);
+
+        System.out.println(num.length);
+    }
+}
