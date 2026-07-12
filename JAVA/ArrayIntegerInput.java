f91cc07f7c0240c7cb366adf9798b476a74c5854
codecraftman77654 <kk1551737@gmail.com>
2026-07-12 10:28:48 +0530

    initial commit

---
+++ JAVA/ArrayIntegerInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class ArrayIntegerInput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5  Numbers : ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array Elements :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        sc.close();
+    }
+}
