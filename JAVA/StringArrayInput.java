d9c9073b5eb751e47c62c2d96360b56f4542a720
codecraftman77654 <kk1551737@gmail.com>
2026-07-12 15:47:53 +0530

    initial commit

---
+++ JAVA/StringArrayInput.java
@@ -0,0 +1,12 @@
+class StringArrayInput {
+    public static void main(String[] args) {
+
+        String[] arr = { "Ram", "Shyam", "Ghanshyam", "Ravan", "Indrajeet" };
+
+        System.out.println("Names are : ");
+
+        for (int i = 0; i <= arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
