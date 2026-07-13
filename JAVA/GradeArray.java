6f0926744c6332d7fc95dbad950a69781b9fb4a7
codecraftman77654 <kk1551737@gmail.com>
2026-07-13 21:30:42 +0530

    initial commit

---
+++ JAVA/GradeArray.java
@@ -0,0 +1,12 @@
+class GradeArray {
+
+    public static void main(String[] args) {
+
+        char[] grade = { 'A', 'B', 'C', 'D' };
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(grade[i]);
+        }
+
+    }
+}
