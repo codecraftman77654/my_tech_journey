c8c8ecbe40cb26670cffc4b482710e2fc2e8772f
codecraftman77654 <kk1551737@gmail.com>
2026-07-20 20:12:36 +0530

    initial commit

---
+++ practice/array.java
@@ -0,0 +1,12 @@
+class array {
+    public static void main(String[] args) {
+
+        int[] marks;
+        // int marks = new int[3];
+
+        marks[0] = 75;
+        marks[1] = 80;
+        marks[2] = 85;
+        System.out.println(marks[0]);
+    }
+}
