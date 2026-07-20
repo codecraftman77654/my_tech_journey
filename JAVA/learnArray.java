85795f88d694f45a7c3361bcd6dc44d7efd3894d
codecraftman77654 <kk1551737@gmail.com>
2026-07-20 20:25:48 +0530

    initial commit

---
+++ JAVA/learnArray.java
@@ -0,0 +1,10 @@
+class learnArray {
+
+    public static void main(String[] args) {
+        int marks[] = { 3, 5, 8, 10 };
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+    }
+}
