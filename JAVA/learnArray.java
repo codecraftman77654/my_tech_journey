e27f454d1d81d4a99042451294f146c3c0bbf715
codecraftman77654 <kk1551737@gmail.com>
2026-07-20 21:09:05 +0530

    initial commit

--- JAVA/learnArray.java
+++ JAVA/learnArray.java
@@ -2,9 +2,13 @@ class learnArray {
 
     public static void main(String[] args) {
         int marks[] = { 3, 5, 8, 10 };
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
-        System.out.println(marks[3]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        // System.out.println(marks[3]);
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+        System.out.println("Array length is " + marks.length);
     }
 }
