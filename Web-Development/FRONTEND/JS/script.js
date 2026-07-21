bfe9d709e85cfbc0c5024e20ce2579603884569c
codecraftman77654 <kk1551737@gmail.com>
2026-07-21 12:52:10 +0530

    initial commit

--- Web-Development/FRONTEND/JS/script.js
+++ Web-Development/FRONTEND/JS/script.js
@@ -1,3 +1,54 @@
-console.log("Welcome Chief");
-console.log(10);
-console.log(true);  
+// console.log("Welcome Chief");
+// console.log(10);
+// console.log(true);  
+
+// let a=10;
+// let b=20;
+// console.log(a+b);
+
+//datatypes//
+
+// let  age =22;
+// console.log(age);
+
+// let name = "Kuldeep";
+// console.log(name);
+
+// let isStudent=true;
+// console.log(isStudent);
+
+// let birth;
+// console.log(birth);
+
+// let user =null;
+// console.log(user);
+
+// let big = 123158461465989846665656446549856164435949419496994165651877465984n;
+// console.log(big);
+
+
+// let student = {
+//     name : "kuldeep",
+//     age:22
+// };
+// console.log(student);
+
+// let marks = [90,85,95 ];
+// console.log(marks);
+
+// function great(){
+//     console.log("Hello");
+// }
+
+
+let a = 10;
+let b = "10";
+let c = true;
+let d;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof null);
+
