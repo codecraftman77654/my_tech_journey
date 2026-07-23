dae6998065919832c0573694c1da6dbc729414f8
codecraftman77654 <kk1551737@gmail.com>
2026-07-23 11:25:14 +0530

    initial commit

---
+++ JAVA/linearSearch.java
@@ -0,0 +1,18 @@
+class linearSearch {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40, 50 };
+        int key = 40;
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("Element Found");
+        } else {
+            System.out.println("Element Not Found.");
+        }
+    }
+}
