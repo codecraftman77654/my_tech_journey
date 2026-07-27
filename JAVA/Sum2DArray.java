88b6ddcc3e4c713ea06f871480811399cb05ea56
codecraftman77654 <kk1551737@gmail.com>
2026-07-27 14:28:37 +0530

    corrected class name

--- JAVA/Sum2DArray.java
+++ JAVA/Sum2DArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class D2Array {
+class Sum2DArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] arr = new int[3][3];
