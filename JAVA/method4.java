d0715c5ea4e104c2b0e56ca44c641b7b94e463d1
codecraftman77654 <kk1551737@gmail.com>
2026-07-28 16:27:31 +0530

    initial commit

---
+++ JAVA/method4.java
@@ -0,0 +1,14 @@
+class lang {
+    static void java() {
+        System.out.println("This is java");
+    }
+
+    static void python() {
+        System.out.println("This is python.");
+    }
+
+    public static void main(String[] args) {
+        python();
+        java();
+    }
+}
