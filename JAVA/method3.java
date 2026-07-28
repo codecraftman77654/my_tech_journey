fe27875571b1edf583017cd3a890158f53597cfa
codecraftman77654 <kk1551737@gmail.com>
2026-07-28 16:18:59 +0530

    initial commit

---
+++ JAVA/method3.java
@@ -0,0 +1,11 @@
+class method {
+    static void line() {
+        System.out.println("===================");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Numerator");
+        line();
+        System.out.println("Denominator");
+    }
+}
