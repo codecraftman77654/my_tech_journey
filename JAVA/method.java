fe7c85923cbad3bd43d4ab019b37775693ebebfb
codecraftman77654 <kk1551737@gmail.com>
2026-07-28 16:00:53 +0530

    initial commit

---
+++ JAVA/method.java
@@ -0,0 +1,11 @@
+class Demo {
+    static void welcome() {
+        System.out.println("Welcome to Java World");
+    }
+
+    public static void main(String[] args) {
+        welcome();
+        welcome();
+        welcome();
+    }
+}
