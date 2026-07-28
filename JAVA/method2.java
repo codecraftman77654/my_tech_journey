5913fa834b8e76ec30c4b027f39ff34dbf78c5b8
codecraftman77654 <kk1551737@gmail.com>
2026-07-28 16:08:46 +0530

    initial commit

---
+++ JAVA/method2.java
@@ -0,0 +1,13 @@
+class greet {
+    static void morning() {
+        System.out.println("Good Morning");
+    }
+
+    public static void main(String[] args) {
+        morning();
+        morning();
+        morning();
+        morning();
+        morning();
+    }
+}
