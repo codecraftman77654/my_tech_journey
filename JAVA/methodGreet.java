de508f595021c5b30b624a39a2d593f68698a645
codecraftman77654 <kk1551737@gmail.com>
2026-07-30 09:20:55 +0530

    initial commit

---
+++ JAVA/methodGreet.java
@@ -0,0 +1,9 @@
+class mrthodGreet {
+    static void greet(String name) {
+        System.out.println("Hello" + name);
+    }
+
+    public static void main(String[] args) {
+        greet(" kuldeep");
+    }
+}
