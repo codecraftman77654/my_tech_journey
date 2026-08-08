b7fffb9d1bfc63bc0a29899f078732c5d92be7c5
codecraftman77654 <kk1551737@gmail.com>
2026-08-08 22:31:18 +0530

    added few more stylings

--- MarvelUniverse/js/app.js
+++ MarvelUniverse/js/app.js
@@ -14,3 +14,55 @@ if(searchInput){
         });
     });
 }
+
+
+// ACTIVE NAVIGATION LINK
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+const currentPage = window.location.pathname.split("/").pop();
+
+navLinks.forEach(function(link){
+
+    const linkPage = link.getAttribute("href");
+
+    if(
+        linkPage === currentPage ||
+        (currentPage === "hero.html" && linkPage === "heroes.html")
+    ){
+        link.classList.add("active");
+    }
+
+});
+
+// HERO  FILTER
+ const filterButtons = document.querySelectorAll(".filter-btn");
+ const heroCards = document.querySelectorAll(".hero-card");
+
+ filterButtons.forEach(function(button){
+    button.addEventListener("click", function(){
+        const selectedFilter = button.dataset.filter;
+
+        // change active button
+
+        filterButtons.forEach(function(btn){
+            btn.classList.remove("active");
+        });
+        button.classList.add("active");
+
+        // filter hero cards
+
+        heroCards.forEach(function(card){
+            const categories = card.dataset.category;
+
+            if(
+                selectedFilter === "all" ||
+                categories.includes(selectedFilter)
+            ){
+                card.style.display = "block";
+            }else {
+                card.style.display = "none";
+            }
+        });
+    });
+ });
