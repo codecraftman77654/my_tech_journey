fc73f2a63c9e2d2a880bdb15c0f6c624517cc79a
codecraftman77654 <kk1551737@gmail.com>
2026-09-09 20:43:53 +0530

    initial commit

---
+++ Python/task3.py
@@ -0,0 +1,13 @@
+hours = int(input("How many hours did you study today ? "))
+
+if hours >= 8 :
+    print("Excellent! you crushed today's target.")
+
+elif 7 >= hours >= 5 :
+    print("Good work, but you can push harder.")
+
+elif 4>= hours >=1 :
+    print("you need to be more disciplined.")
+
+else :
+    print("You did nothing.")
