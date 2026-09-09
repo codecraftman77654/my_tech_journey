aab5fb3002c5ca4b8c697fc8e2bdcdf6b883fa4d
codecraftman77654 <kk1551737@gmail.com>
2026-09-09 17:56:23 +0530

    initial commit

---
+++ Python/task1.py
@@ -0,0 +1,10 @@
+name = input("What is your name ?")
+age = int(input("What is your age ?"))
+income = int(input("What is your current monthly income ?"))
+desired = int(input("What is your desired monthly income ?"))
+
+print("Hello", name)
+print ("you are", age, "years old.")
+print("your current monthly income is", income)
+print("your target monthly income is ", desired)
+print("Your journey starts today.")
